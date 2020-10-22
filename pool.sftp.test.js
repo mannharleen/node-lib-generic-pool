31b8d7965cc96b4271fa812df773bf5709a662b5
Harleen Mann <a@a.com>
2020-10-22 22:16:37 +1100

    v 1.0.3 - added skiiping test if underlying lib or its config is not available - removed an accidental comment - increased jest timeout to 1 min

--- pool.sftp.test.js
+++ pool.sftp.test.js
@@ -1,47 +1,54 @@
 /**
  * tests against "ssh2-sftp-client"
  */
-const Client = require('ssh2-sftp-client');
-const config = require('./_sftpConfig.js')
+
+let Client, config, connSettings, createFunc, destroyFunc, validateConnFunc
+
+try {
+    Client = require('ssh2-sftp-client');
+    config = require('./_sftpConfig.js');
+
+    connSettings = [config.connSettings];
+    createFunc = async (connSettings) => {
+        let sftp = new Client()
+        await sftp.connect(connSettings)
+        return sftp
+    }
+    destroyFunc = async (conn) => {
+        await conn.end()
+    }
+    validateConnFunc = async (conn) => {
+        try {
+            await conn.cwd()
+            return true
+        } catch (e) {
+            return false
+        }
+    };
+
+} catch (e) {
+    console.warn(`Skipping test file named: ${__filename.slice(__dirname.length + 1)}. Reason:`, e.message);
+    test = test.skip
+}
 
 // standard requires for all test suites
 const Pool = require('./index');
 const tests = require('./tests')
 
-
-let connSettings = [config.connSettings];
-let createFunc = async (connSettings) => { 
-    let sftp = new Client()
-    await sftp.connect(connSettings)
-    return sftp
-}
-let destroyFunc = async (conn) => { 
-    await conn.end()
-}
-let validateConnFunc = async (conn) => {
-    try {
-        await conn.cwd()
-        return true
-    } catch (e) {
-        return false
-    }
-};
-
 // standard tests for all test suites
-// !!! uncomment
-// tests(Pool, connSettings, createFunc, destroyFunc, validateConnFunc);
+tests(Pool, connSettings, createFunc, destroyFunc, validateConnFunc);
 
 // additional tests
 test("list dir that exists", async () => {
-    let pool = await Pool(connSettings, createFunc, destroyFunc, validateConnFunc);    
+    let pool = await Pool(connSettings, createFunc, destroyFunc, validateConnFunc);
     let conn = await pool.acquire();
-    await expect(conn.list(config.homedir)).resolves.not.toBeNull()    
+    await expect(conn.list(config.homedir)).resolves.not.toBeNull()
     await pool.destroy()
 })
 
 test("list dir that does not exists", async () => {
-    let pool = await Pool(connSettings, createFunc, destroyFunc, validateConnFunc);    
+    let pool = await Pool(connSettings, createFunc, destroyFunc, validateConnFunc);
     let conn = await pool.acquire();
-    await expect(conn.list('something really random like this text')).rejects.not.toBeNull()    
+    await expect(conn.list('something really random like this text')).rejects.not.toBeNull()
     await pool.destroy()
 })
